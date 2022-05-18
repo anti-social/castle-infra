80d07f74b044e86e0964804a969d0286e8ed9e86
System administrator <Olexandr Koval>
2022-05-18 20:04:48 +0300

    Install podman

--- configuration.nix
+++ configuration.nix
@@ -176,6 +176,12 @@ in
     wget
   ];
 
+  virtualisation = {
+    podman = {
+      enable = true;
+    };
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
