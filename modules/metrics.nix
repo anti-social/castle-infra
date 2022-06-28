d6b5b15661990ca80e363ebc571318444fffecc8
Alexander Koval <kovalidis@gmail.com>
2022-06-29 00:57:58 +0300

    Run grafana using podman

--- modules/metrics.nix
+++ modules/metrics.nix
@@ -2,23 +2,40 @@
 
 with lib;
 
-{
+let
+  grafana_version = "9.0.1";
+  grafana_port = 3000;
+  grafana_domain = "grafana.castle";
+in {
   config = {
     services.victoriametrics = {
       enable = true;
       listenAddress = "127.0.0.1:8428";
     };
 
-    services.grafana = rec {
-      enable = true;
-      domain = "grafana.castle";
-      port = 5000;
-      addr = "127.0.0.1";
-      rootUrl = "http://${domain}";
+    virtualisation.oci-containers.containers.grafana = {
+      image = "docker.io/grafana/grafana-oss:${grafana_version}";
+      environment = {
+        TZ = config.time.timeZone;
+        GF_SERVER_HTTP_ADDR = "127.0.0.1";
+        GF_SERVER_DOMAIN = grafana_domain;
+        GF_SERVER_ROOT_URL = "http://${grafana_domain}";
+      };
+      # user = "grafana:grafana";
+      volumes = [
+        "grafana:/var/lib/grafana"
+      ];
+
+      # ports = [
+      #   "127.0.0.1:${toString grafana_port}:${toString grafana_port}"
+      # ];
+      extraOptions = [
+        "--network=host"
+      ];
     };
-    services.nginx.virtualHosts.${config.services.grafana.domain} = {
+    services.nginx.virtualHosts.${grafana_domain} = {
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyPass = "http://127.0.0.1:${toString grafana_port}";
         extraConfig = ''
           proxy_set_header Host $host;
         '';
