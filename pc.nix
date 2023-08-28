51512ebe7b4533ef1921a43e9938dae84d9ed37e
Alexander Koval <kovalidis@gmail.com>
2023-08-28 19:39:40 +0300

    pc: rye 0.7.0 -> 0.11.0

--- pc.nix
+++ pc.nix
@@ -278,19 +278,20 @@ in {
     rye = ({ lib, fetchFromGithub, rustPlatform, pkgconfig, openssl, libiconv, git }:
       rustPlatform.buildRustPackage rec {
         pname = "rye";
-        version = "0.7.0";
+        version = "0.11.0";
 
         src = fetchFromGitHub {
           owner = "mitsuhiko";
           repo = pname;
           rev = version;
-          sha256 = "m4vJOwcxVTC+oglcpsE5+wip5EJEv7vlhyYbcBZeXQo=";
+          sha256 = "00Q+qvK1fq9CGb6umtCiUJZZ1M5LMxiSIM3/s7eOumM=";
         };
 
         cargoLock = {
           lockFile = ./rye/Cargo.lock;
           outputHashes = {
             "dialoguer-0.10.4" = "sha256-WDqUKOu7Y0HElpPxf2T8EpzAY3mY8sSn9lf0V0jyAFc=";
+            "monotrail-utils-0.0.1" = "sha256-4x5jnXczXnToU0QXpFalpG5A+7jeyaEBt8vBwxbFCKQ=";
           };
         };
 
