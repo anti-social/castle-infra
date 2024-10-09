0c17c261964075c8020a8d3755697eceba8ad44d
Alexander Koval <kovalidis@gmail.com>
2024-10-09 11:43:40 +0300

    Add udev rules for stm device in dfu mode and wch-link

--- modules/udev.nix
+++ modules/udev.nix
@@ -4,7 +4,13 @@
   config = {
     services.udev.extraRules = ''
       # ST-Link
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE="660", GROUP="wheel"
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE="660", GROUP="dialout"
+
+      # STMicroelectronics STM Device in DFU Mode
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE="660", GROUP="dialout"
+
+      # WCH-Link
+      SUBSYSTEMS=="usb", ATTRS{idVendor}=="1a86", ATTRS{idProduct}=="8012", MODE="660", GROUP="dialout"
     '';
   };
 }
