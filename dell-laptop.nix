720a7f60d92b224ff7aaff3c462662c7c3121ee8
Alexander Koval <kovalidis@gmail.com>
2025-03-15 23:31:27 +0200

    dell-laptop: enable nix-ld

--- dell-laptop.nix
+++ dell-laptop.nix
@@ -308,6 +308,10 @@ in {
     };
   };
 
+  programs.nix-ld = {
+    enable = true;
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
