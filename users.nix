c46d4c7fe43cd18cfe929a4f297a118a821714d4
Alexander Koval <kovalidis@gmail.com>
2025-07-17 19:51:18 +0300

    Add user to podman group

--- users.nix
+++ users.nix
@@ -4,7 +4,16 @@
   alexk = {
     uid = 1000;
     isNormalUser = true;
-    extraGroups = [ "audio" "dialout" "libvirtd" "networkmanager" "video" "wheel" "wireshark" ];
+    extraGroups = [
+      "audio"
+      "dialout"
+      "libvirtd"
+      "networkmanager"
+      "podman"
+      "video"
+      "wheel"
+      "wireshark"
+    ];
     shell = pkgs.zsh;
     linger = true;
   };
