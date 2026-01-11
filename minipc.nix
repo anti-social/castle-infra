941a8582c3650b0b72d38fef4199a75a37dd322e
Alexander Koval <kovalidis@gmail.com>
2026-01-12 00:52:56 +0200

    minipc: fix betaflight-configurator

--- minipc.nix
+++ minipc.nix
@@ -159,6 +159,17 @@ in {
     enable = true;
   };
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      nwjs = prev.nwjs.overrideAttrs {
+        version = "0.84.0";
+        src = prev.fetchurl {
+          url = "https://dl.nwjs.io/v0.84.0/nwjs-v0.84.0-linux-x64.tar.gz";
+          hash = "sha256-VIygMzCPTKzLr47bG1DYy/zj0OxsjGcms0G1BkI/TEI=";
+        };
+      };
+    })
+  ];
   # nixpkgs.overlays = (import ./overlays.nix) { pkgs = pkgs; };
 
   # List packages installed in system profile. To search, run:
